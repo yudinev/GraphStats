1fc6dbfd893e93775de1db33527c781ae3a32e48
semochkin <stas.semochkin@gmail.com>
2014-12-26 18:22:50 +0700

    code refactoring.

--- src/com/asoiu/simbigraph/algorithms/shortestpath/ParallelDistanceStatistics.java
+++ src/com/asoiu/simbigraph/algorithms/shortestpath/ParallelDistanceStatistics.java
@@ -1,72 +1,70 @@
 package com.asoiu.simbigraph.algorithms.shortestpath;
 
+import edu.uci.ics.jung.graph.Hypergraph;
+
 import java.util.Collection;
 import java.util.function.Function;
-import edu.uci.ics.jung.graph.Hypergraph;
 
 /**
  * This is parallel version of
  * edu.uci.ics.jung.algorithms.shortestpath.DistanceStatistics class.
- * 
- * @see edu.uci.ics.jung.algorithms.shortestpath.DistanceStatistics
+ *
  * @author Andrey Kurchanov
+ * @see edu.uci.ics.jung.algorithms.shortestpath.DistanceStatistics
  */
 public class ParallelDistanceStatistics<V, E> {
-	Collection<V> vertices;
-	Object[] eccs;
+    private Hypergraph<V, E> graph;
+    private Collection<V> vertices;
+    private Object[] eccs;
 
-	/**
-	 * Saves eccentricities of the <code>g</code> vertices into
-	 * <code>eccs</code> array. The eccentricity is defined to be the maximum,
-	 * over all pairs of vertices <code>u,v</code>, of the length of the
-	 * shortest path from <code>u</code> to <code>v</code>. If the graph is
-	 * disconnected (that is, some vertex is not reachable from another
-	 * vertices), the value associated with the vertex into <code>eccs</code>
-	 * array will be <code>Double.POSITIVE_INFINITY</code>.
-	 * 
-	 * This method uses Function and Parallel Stream features of JDK 1.8 for
-	 * parallel execution.
-	 */
-	public void getEccentricities(Hypergraph<V, E> g,
-			ParallelUnweightedShortestPath<V, E> d) {
-		vertices = g.getVertices();
-		Function<V, Double> f_inner = (v) -> {
-			int di = d.getEccentricity(v);
-			if (di == Integer.MAX_VALUE)
-				return Double.POSITIVE_INFINITY;
-			else
-				return (double) di;
-		};
-		eccs = vertices.parallelStream().map(f_inner).sorted().toArray();
-	}
+    public ParallelDistanceStatistics(Hypergraph<V, E> graph) {
+        this.graph = graph;
+        calculateEccentricities(this.graph);
+    }
 
-	/**
-	 * Returns the diameter of <code>g</code>, ignoring edge weights. The
-	 * diameter is defined to be the maximum, over all pairs of vertices
-	 * <code>u,v</code>, of the length of the shortest path from <code>u</code>
-	 * to <code>v</code>, or <code>Double.POSITIVE_INFINITY</code> if any of
-	 * these distances do not exist.
-	 * 
-	 * @see #getEccentricities(Hypergraph, ParallelUnweightedShortestPath)
-	 */
-	public double diameter(Hypergraph<V, E> g) {
-		if (eccs == null)
-			getEccentricities(g, new ParallelUnweightedShortestPath<V, E>(g));
-		return (Double) eccs[eccs.length - 1];
-	}
+    /**
+     * Saves eccentricities of the <code>g</code> vertices into
+     * <code>eccs</code> array. The eccentricity is defined to be the maximum,
+     * over all pairs of vertices <code>u,v</code>, of the length of the
+     * shortest path from <code>u</code> to <code>v</code>. If the graph is
+     * disconnected (that is, some vertex is not reachable from another
+     * vertices), the value associated with the vertex into <code>eccs</code>
+     * array will be <code>Double.POSITIVE_INFINITY</code>.
+     * <p>
+     * This method uses Function and Parallel Stream features of JDK 1.8 for
+     * parallel execution.
+     */
+    private void calculateEccentricities(Hypergraph<V, E> g) {
+        ParallelUnweightedShortestPath<V, E> d = new ParallelUnweightedShortestPath<>(g);
+        this.vertices = g.getVertices();
+        Function<V, Double> f_inner = (v) -> {
+            int di = d.getEccentricity(v);
+            return (di == Integer.MAX_VALUE) ? Double.POSITIVE_INFINITY : (double)di;
+        };
+        this.eccs = this.vertices.parallelStream().map(f_inner).sorted().toArray();
+    }
 
-	/**
-	 * Returns the radius of <code>g</code>, ignoring edge weights. The radius
-	 * is defined to be the minimum, over all pairs of vertices <code>u,v</code>
-	 * , of the length of the shortest path from <code>u</code> to
-	 * <code>v</code>, or <code>Double.POSITIVE_INFINITY</code> if any of these
-	 * distances do not exist.
-	 * 
-	 * @see #getEccentricities(Hypergraph, ParallelUnweightedShortestPath)
-	 */
-	public double radius(Hypergraph<V, E> g) {
-		if (eccs == null)
-			getEccentricities(g, new ParallelUnweightedShortestPath<V, E>(g));
-		return (Double) eccs[0];
-	}
+    /**
+     * The getDiameter is defined to be the maximum, over all pairs of vertices
+     * <code>u,v</code>, of the length of the shortest path from <code>u</code>
+     * to <code>v</code>, or <code>Double.POSITIVE_INFINITY</code> if any of
+     * these distances do not exist.
+     *
+     * @return the getDiameter of <code>g</code>, ignoring edge weights.
+     */
+    public double getDiameter() {
+        return (Double)eccs[eccs.length - 1];
+    }
+
+    /**
+     * The getRadius is defined to be the minimum, over all pairs of vertices <code>u,v</code>
+     * , of the length of the shortest path from <code>u</code> to
+     * <code>v</code>, or <code>Double.POSITIVE_INFINITY</code> if any of these
+     * distances do not exist.
+     *
+     * @return the getRadius of <code>g</code>, ignoring edge weights.
+     */
+    public double getRadius() {
+        return (Double)eccs[0];
+    }
 }
