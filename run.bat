3a29803664f00dc74796050a078c426c456b3c3a
AKurchanov <andrey.kurchanov@yandex.ru>
2015-02-23 23:14:55 +0600

    run.bat has been added.

---
+++ run.bat
@@ -0,0 +1,6 @@
+java -server -XX:+DoEscapeAnalysis -XX:+AggressiveOpts -Xms2048m -Xmx2048m -jar C:\Users\Work\workspace\GraphStats\bin\dist\GraphStats.jar C:\Users\Work\workspace\GraphStats\RealGraphs\p2p-Gnutella08.net
+java -server -XX:+DoEscapeAnalysis -XX:+AggressiveOpts -Xms2048m -Xmx2048m -jar C:\Users\Work\workspace\GraphStats\bin\dist\GraphStats.jar C:\Users\Work\workspace\GraphStats\RealGraphs\p2p-Gnutella08.net
+java -server -XX:+DoEscapeAnalysis -XX:+AggressiveOpts -Xms2048m -Xmx2048m -jar C:\Users\Work\workspace\GraphStats\bin\dist\GraphStats.jar C:\Users\Work\workspace\GraphStats\RealGraphs\p2p-Gnutella08.net
+java -server -XX:+DoEscapeAnalysis -XX:+AggressiveOpts -Xms2048m -Xmx2048m -jar C:\Users\Work\workspace\GraphStats\bin\dist\GraphStats.jar C:\Users\Work\workspace\GraphStats\RealGraphs\p2p-Gnutella08.net
+java -server -XX:+DoEscapeAnalysis -XX:+AggressiveOpts -Xms2048m -Xmx2048m -jar C:\Users\Work\workspace\GraphStats\bin\dist\GraphStats.jar C:\Users\Work\workspace\GraphStats\RealGraphs\p2p-Gnutella08.net
+pause
