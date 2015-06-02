8ff8fa8d46105e9be22aa09af8acc4d2fa2a5ca4
Andrey Kurchanov <andrey.kurchanov@yandex.ru>
2015-06-03 01:08:07 +0600

    From now AdjacencyListGraph also stores list of the edges as well as adjacency list. Furthermore, all methods of edu.uci.ics.jung.graph.Graphwere implemented.

--- src/main/java/com/asoiu/simbigraph/graph/AdjacencyListGraph.java
+++ src/main/java/com/asoiu/simbigraph/graph/AdjacencyListGraph.java
@@ -2,49 +2,78 @@ package com.asoiu.simbigraph.graph;
 
 import edu.uci.ics.jung.graph.AbstractGraph;
 import edu.uci.ics.jung.graph.util.EdgeType;
-import sun.reflect.generics.reflectiveObjects.NotImplementedException;
+import edu.uci.ics.jung.graph.util.Pair;
 
 import java.util.*;
 
 /**
  * An implementation of <code>edu.uci.ics.jung.graph.Graph</code> which
- * represents the graph as an adjacency list and permits directed or undirected
- * edges only.
+ * represents the graph as an adjacency list and list of the edges. The
+ * implementation permits directed or undirected edges only.
  *
  * @author Andrey Kurchanov
  */
 public class AdjacencyListGraph<V,E> extends AbstractGraph<V,E> {
 
-    private Map<V, List<V>> mGraph;
+    private Map<V, List<V>> vertices;
+    
+    private Map<E, Pair<? extends V>> edges;
 
     private EdgeType edgeType;
 
     public AdjacencyListGraph() {
-        mGraph = new HashMap<>();
+        vertices = new HashMap<>();
+        edges = new HashMap<E, Pair<? extends V>>();
+    }
+    
+    @Override
+    public boolean addVertex(V vertex) {
+    	if (!containsVertex(vertex)) {
+            // Add the vertex with an empty list of outgoing edges.
+    		vertices.put(vertex, new ArrayList<>());
+    	}
+        return true;
+    }
+    
+    @Override
+    public boolean addEdge(E edge, Pair<? extends V> endpoints, EdgeType edgeType) {
+        if (!containsVertex(endpoints.getFirst()) || !containsVertex(endpoints.getSecond())) {
+        	return false;
+        }
+    	
+    	this.edgeType = edgeType;
+
+        if (!vertices.get(endpoints.getFirst()).contains(endpoints.getSecond())) {
+        	// Add the edge.
+        	if (edgeType == EdgeType.UNDIRECTED) {
+        		vertices.get(endpoints.getFirst()).add(endpoints.getSecond());
+        		vertices.get(endpoints.getSecond()).add(endpoints.getFirst());
+        	} else {
+        		vertices.get(endpoints.getFirst()).add(endpoints.getSecond());
+        	}
+        	edges.put(edge, endpoints);
+        }
+        return true;
     }
 
     @Override
     public Collection<V> getVertices() {
-        return new ArrayList<>(mGraph.keySet());
+        return new ArrayList<>(vertices.keySet());
     }
 
     @Override
     public boolean containsVertex(V vertex) {
-        return mGraph.containsKey(vertex);
-    }
-
-    @Override
-    public int getEdgeCount() {
-        int edgeCount = 0;
-        for (List<V> list : mGraph.values()) {
-            edgeCount += list.size();
-        }
-        return edgeType == EdgeType.UNDIRECTED ? edgeCount / 2 : edgeCount;
+        return vertices.containsKey(vertex);
     }
 
     @Override
     public int getVertexCount() {
-        return mGraph.size();
+    	return vertices.size();
+    }
+
+    @Override
+    public int getEdgeCount() {
+    	return edges.size();
     }
 
     @Override
@@ -54,15 +83,6 @@ public class AdjacencyListGraph<V,E> extends AbstractGraph<V,E> {
             neighbors.addAll(getPredecessors(vertex));
         }
         return neighbors;
-    }
-
-    @Override
-    public boolean addVertex(V vertex) {
-        // Add the vertex with an empty list of outgoing edges.
-    	if (!containsVertex(vertex)) {
-    		mGraph.put(vertex, new ArrayList<>());
-    	}
-        return true;
     }
 
     @Override
@@ -77,106 +97,109 @@ public class AdjacencyListGraph<V,E> extends AbstractGraph<V,E> {
 
     @Override
     public int getEdgeCount(EdgeType edge_type) {
-        return getEdgeCount();
+    	return edge_type == this.edgeType ? getEdgeCount() : 0;
     }
 
     @Override
     public Collection<V> getPredecessors(V vertex) {
-        List<V> predecessors = new ArrayList<>(mGraph.size() / 2);
-        for (Map.Entry<V, List<V>> pair : mGraph.entrySet()) {
+    	Collection<V> predecessors = new ArrayList<>();
+        for (Map.Entry<V, List<V>> pair : vertices.entrySet()) {
             if (pair.getValue().contains(vertex)) {
                 predecessors.add(pair.getKey());
             }
         }
-        return predecessors.size() == 0 ? new ArrayList<>() : predecessors;
+        return predecessors;
     }
 
     @Override
     public Collection<V> getSuccessors(V vertex) {
-        return mGraph.get(vertex);
+        return new ArrayList<>(vertices.get(vertex));
     }
 
     @Override
-    public boolean addEdge(E edge, edu.uci.ics.jung.graph.util.Pair<? extends V> endpoints, EdgeType edgeType) {
-        this.edgeType = edgeType;
-
-        if (mGraph.get(endpoints.getFirst()).contains(endpoints.getSecond()) == false) {
-        	// Add the edge.
-        	if (edgeType == EdgeType.UNDIRECTED) {
-        		mGraph.get(endpoints.getFirst()).add(endpoints.getSecond());
-        		mGraph.get(endpoints.getSecond()).add(endpoints.getFirst());
-        	} else {
-        		mGraph.get(endpoints.getFirst()).add(endpoints.getSecond());
-        	}
-        }
+    public Collection<E> getEdges() {
+        return new ArrayList<>(edges.keySet());
+    }
+    
+    @Override
+    public boolean containsEdge(E edge) {
+        return edges.containsKey(edge);
+    }
+    
+    @Override
+    public boolean removeVertex(V vertex) {
+        vertices.remove(vertex);
         return true;
     }
-
-    // NOT IMPLEMENTED
-
+    
     @Override
-    public Collection<E> getEdges() throws NotImplementedException {
-        throw new NotImplementedException();
+    public boolean removeEdge(E edge) {
+    	edges.remove(edge);
+        return true;
+    }
+    
+    @Override
+    public Pair<V> getEndpoints(E edge) {
+        return (Pair<V>) edges.get(edge);
+    }
+    
+    @Override
+    public boolean isNeighbor(V v1, V v2) {
+    	return vertices.get(v1).contains(v2);
+    }
+    
+    @Override
+    public Collection<E> getIncidentEdges(V vertex) {
+    	Collection<E> incidentEdges = new ArrayList<>(getOutEdges(vertex));
+    	incidentEdges.addAll(getInEdges(vertex));
+    	return incidentEdges;
+    }
+    
+    @Override
+    public Collection<E> getEdges(EdgeType edge_type) {
+        return edge_type == this.edgeType ? new ArrayList<>(edges.keySet()) : new ArrayList<>();
+    }
+    
+    @Override
+    public Collection<E> getInEdges(V vertex) {
+        List<E> inEdges = new ArrayList<>();
+    	for (E edge : getEdges()) {
+			if (getEndpoints(edge).getSecond() == vertex) {
+				inEdges.add(edge);
+			}
+		}
+    	return inEdges;
     }
 
     @Override
-    public boolean containsEdge(E edge) throws NotImplementedException {
-        throw new NotImplementedException();
+    public Collection<E> getOutEdges(V vertex) {
+    	List<E> outEdges = new ArrayList<>();
+    	for (E edge : getEdges()) {
+			if (getEndpoints(edge).getFirst() == vertex) {
+				outEdges.add(edge);
+			}
+		}
+    	return outEdges;
+    }
+    
+    @Override
+    public V getSource(E directed_edge) {
+        return edges.get(directed_edge).getFirst();
     }
 
     @Override
-    public Collection<E> getIncidentEdges(V vertex) throws NotImplementedException {
-        throw new NotImplementedException();
+    public V getDest(E directed_edge) {
+    	return edges.get(directed_edge).getSecond();
     }
 
     @Override
-    public boolean removeVertex(V vertex) throws NotImplementedException {
-        throw new NotImplementedException();
+    public boolean isSource(V vertex, E edge) {
+    	return edges.get(edge).getFirst() == vertex;
     }
 
     @Override
-    public boolean removeEdge(E edge) throws NotImplementedException {
-        throw new NotImplementedException();
-    }
-
-    @Override
-    public Collection<E> getEdges(EdgeType edge_type) throws NotImplementedException {
-        throw new NotImplementedException();
-    }
-
-    @Override
-    public Collection<E> getInEdges(V vertex) throws NotImplementedException {
-        throw new NotImplementedException();
-    }
-
-    @Override
-    public Collection<E> getOutEdges(V vertex) throws NotImplementedException {
-        throw new NotImplementedException();
-    }
-
-    @Override
-    public V getSource(E directed_edge) throws NotImplementedException {
-        throw new NotImplementedException();
-    }
-
-    @Override
-    public V getDest(E directed_edge) throws NotImplementedException {
-        throw new NotImplementedException();
-    }
-
-    @Override
-    public boolean isSource(V vertex, E edge) throws NotImplementedException {
-        throw new NotImplementedException();
-    }
-
-    @Override
-    public boolean isDest(V vertex, E edge) throws NotImplementedException {
-        throw new NotImplementedException();
-    }
-
-    @Override
-    public edu.uci.ics.jung.graph.util.Pair<V> getEndpoints(E edge) throws NotImplementedException {
-        throw new NotImplementedException();
+    public boolean isDest(V vertex, E edge) {
+    	return edges.get(edge).getSecond() == vertex;
     }
     
 }
